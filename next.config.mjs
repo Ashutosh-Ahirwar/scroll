c79b927c447def9187ab2d74eb8647e8b528951b
Ashutosh-Ahirwar <ashutoshahirwr@gmail.com>
2025-12-12 22:45:11 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -11,10 +11,29 @@ const nextConfig = {
             value: `
               default-src 'self';
               script-src 'self' 'unsafe-eval' 'unsafe-inline';
-              style-src 'self' 'unsafe-inline';
+              style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
               img-src 'self' blob: data: https:;
-              font-src 'self' data:;
-              connect-src 'self' https://explorer-api.walletconnect.com https://*.walletconnect.com wss://*.walletconnect.com https://cca-lite.coinbase.com https://*.alchemyapi.io https://*.base.org https://base-mainnet.g.alchemy.com https://farcaster.xyz https://client.farcaster.xyz https://warpcast.com https://client.warpcast.com https://wrpcd.net https://*.wrpcd.net https://privy.farcaster.xyz https://privy.warpcast.com https://auth.privy.io https://*.rpc.privy.systems https://cloudflareinsights.com;
+              font-src 'self' data: https://fonts.gstatic.com;
+              connect-src 'self' 
+                https://explorer-api.walletconnect.com 
+                https://*.walletconnect.com 
+                wss://*.walletconnect.com 
+                https://cca-lite.coinbase.com 
+                https://*.alchemyapi.io 
+                https://*.base.org 
+                https://base-mainnet.g.alchemy.com 
+                https://farcaster.xyz 
+                https://client.farcaster.xyz 
+                https://warpcast.com 
+                https://client.warpcast.com 
+                https://wrpcd.net 
+                https://*.wrpcd.net 
+                https://privy.farcaster.xyz 
+                https://privy.warpcast.com 
+                https://auth.privy.io 
+                https://*.rpc.privy.systems 
+                https://cloudflareinsights.com 
+                https://*.browser-intake-datadoghq.com;
               frame-src 'self' https://verify.walletconnect.com;
             `.replace(/\s{2,}/g, ' ').trim(),
           },
