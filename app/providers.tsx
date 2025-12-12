caec2340430c52b859a55a248498c52ee41263db
Ashutosh-Ahirwar <ashutoshahirwr@gmail.com>
2025-12-12 23:01:08 +0530

    changes

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,7 +4,7 @@ import { OnchainKitProvider } from '@coinbase/onchainkit';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { type ReactNode, useState } from 'react';
 import { WagmiProvider } from 'wagmi';
-import { baseSepolia } from 'wagmi/chains';
+import { base } from 'wagmi/chains';
 import { config } from './config';
 
 export function Providers({ children }: { children: ReactNode }) {
@@ -14,7 +14,7 @@ export function Providers({ children }: { children: ReactNode }) {
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
         {/* API Key is optional for testing, but needed for OnchainKit analytics later */}
-        <OnchainKitProvider chain={baseSepolia}>
+        <OnchainKitProvider chain={base}>
           {children}
         </OnchainKitProvider>
       </QueryClientProvider>
